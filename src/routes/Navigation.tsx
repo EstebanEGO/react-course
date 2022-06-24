9f12d83d36fc6ddf58ab41a0b4e50f881d2fd70b
Esteban Eduardo Garcia Ojeda <esteban.e.garcia.o@gmail.com>
2022-06-24 18:59:11 -0500

    rutas anidadas

---
+++ src/routes/Navigation.tsx
@@ -0,0 +1,36 @@
+import { BrowserRouter, Navigate, NavLink, Route, Routes } from "react-router-dom"
+import { routes } from "./routes";
+import logo from "../logo.svg";
+import { Suspense } from "react";
+
+export const Navigation = () => {
+    return (
+        <Suspense fallback={ <span>Loading...</span> }>
+            <BrowserRouter>
+                <div className="main-layout">
+                    <nav>
+                        <img src={ logo }/>
+                        <ul>
+                            { 
+                                routes.map(({ to, name }) => (
+                                    <li key={ to }>
+                                        <NavLink to={ to } className={ ({ isActive }) => isActive ? 'nav-active' : '' }>{ name }</NavLink>                                                 
+                                    </li>
+                                )) 
+                            }    
+                        </ul>
+                    </nav>     
+                    <Routes>
+                        { 
+                            routes.map(({ path, Component }) => (
+                                <Route key={ path } path={ path } element={ <Component />}/>    
+                            )) 
+                        }
+
+                        <Route path="/*" element={ <Navigate to={ routes[0].to } replace/> }/>    
+                    </Routes>           
+                </div>
+            </BrowserRouter>
+        </Suspense>
+    )
+}
