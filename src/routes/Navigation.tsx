ac0bdda25a9e37b0c3b824fa0025b5e8cdbce962
Esteban Eduardo Garcia Ojeda <esteban.e.garcia.o@gmail.com>
2022-06-28 13:58:31 -0500

    component compound

---
+++ src/routes/Navigation.tsx
@@ -0,0 +1,33 @@
+import { BrowserRouter, Navigate, NavLink, Route, Routes } from "react-router-dom"
+import { ShoppingPage } from "../02-component-patterns/pages/ShoppingPage";
+
+import logo from "../logo.svg";
+
+export const Navigation = () => {
+    return (
+        <BrowserRouter>
+            <div className="main-layout">
+                <nav>
+                    <img src={ logo }/>
+                    <ul>
+                        <li>
+                            <NavLink to={ '/' } className={ ({ isActive }) => isActive ? 'nav-active' : '' }>Shopping</NavLink>                                                 
+                        </li>
+                        <li>
+                            <NavLink to={ '/about' } className={ ({ isActive }) => isActive ? 'nav-active' : '' }>About</NavLink>                                                 
+                        </li>
+                        <li>
+                            <NavLink to={ '/users' } className={ ({ isActive }) => isActive ? 'nav-active' : '' }>Users</NavLink>                                                 
+                        </li>
+                    </ul>
+                </nav>     
+                <Routes>
+                    <Route path={ '/about' } element={ <h1>About</h1>}/>    
+                    <Route path={ '/users' } element={ <h1>Users</h1>}/>    
+                    <Route path={ '/' } element={ <ShoppingPage /> }/>    
+                    <Route path="/*" element={ <Navigate to={ '/' } replace/> }/>    
+                </Routes>           
+            </div>
+        </BrowserRouter>
+    )
+}
